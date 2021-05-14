8ff642847cec1f9302c65ebfc98d292b8113667d
asyraf.rmc <asyraf.rmc@gmail.com>
2021-05-14 21:50:06 +0800

    moremore #hotFix

--- src/server/scripts/newman.report.sh
+++ src/server/scripts/newman.report.sh
@@ -4,4 +4,5 @@
 newman run https://www.getpostman.com/collections/8c4f3dcff34a226e6cc4 \
   --environment 'apis/test/LOKETLA Environment.postman_environment.json' \
   --reporters cli,htmlextra \
+  --suppress-exit-code \
   --reporter-htmlextra-export 'apis/test/report.html' \
--- src/server/scripts/newman.sh
+++ src/server/scripts/newman.sh
@@ -3,3 +3,4 @@
 
 newman run https://www.getpostman.com/collections/8c4f3dcff34a226e6cc4 \
   --environment 'apis/test/LOKETLA Environment.postman_environment.json' \
+  --suppress-exit-code \

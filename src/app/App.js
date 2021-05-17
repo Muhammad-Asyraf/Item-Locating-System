9416dd230f2a7116874b9cb80f1d5253dcbd0240
danishrashidin <danishrashidin@gmail.com>
2021-05-17 16:27:05 +0800

    Fixed Expo configuirations on Docker #hotFix

--- src/app/App.js
+++ src/app/App.js
@@ -1,11 +1,16 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { TextInput } from 'react-native-paper';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <TextInput
+      label="Email"
+    />
+      <Text>Hello World!</Text>
+
       <StatusBar style="auto" />
     </View>
   );
@@ -14,8 +19,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });

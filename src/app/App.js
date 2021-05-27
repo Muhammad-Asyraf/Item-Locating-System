1552a905a63b5993b6f3517579029619ac75f18f
danishrashidin <danishrashidin@gmail.com>
2021-05-27 17:09:19 +0800

    docker install error #hotFix

--- src/app/App.js
+++ src/app/App.js
@@ -1,26 +1,45 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
-import { TextInput } from 'react-native-paper';
+import { StyleSheet, ScrollView, View, Text } from "react-native";
+import { Provider as PaperProvider } from "react-native-paper";
+import Theme from "./styles/theme";
+import AppLoading from "expo-app-loading";
+import {
+  useFonts,
+  Inter_500Medium as interMedium,
+  Inter_700Bold as interBold,
+  Inter_800ExtraBold as interBoldExtra,
+} from "@expo-google-fonts/inter";
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs"
+import { Login } from './screens/Login'
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <TextInput
-      label="Email"
-    />
-      <Text>Hello World!</Text>
+  // Fonts to be used
+  let [fontsLoaded] = useFonts({
+    interMedium,
+    interBold,
+    interBoldExtra,
+  });
 
-      <StatusBar style="auto" />
-    </View>
-  );
+  const  AuthTabs = createBottomTabNavigator();
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  } else {
+    return (
+      <NavigationContainer>
+        <PaperProvider theme={Theme()}>
+          <AuthTabs.Navigator>
+            <AuthTabs.Screen name="Login" component={Login}/>
+          </AuthTabs.Navigator>
+
+        </PaperProvider>
+      </NavigationContainer>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
+  footer: {},
 });

562c81bc617b976bd9f3e0fe30f2016cd4223112
asyraf.rmc <asyraf.rmc@gmail.com>
2021-06-07 09:20:33 +0800

    setup initial Dashboard. #LOKET-87

--- src/client/src/components/dashboard/Dashboard.js
+++ src/client/src/components/dashboard/Dashboard.js
@@ -1,39 +1,73 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useDispatch } from 'react-redux';
-import { useHistory } from 'react-router-dom';
+import { withRouter, Switch, Route } from 'react-router-dom';
 
-import Button from '@material-ui/core/Button';
+import { makeStyles } from '@material-ui/core/styles';
 
-import {
-  clearActiveUser,
-  verifying,
-  verified,
-  clearState,
-} from '../../redux/features/authSlice';
-import { auth } from '../../firebase';
+import Drawer from './Drawer';
+import AppBars from './AppBars';
 
-export default function Dashboard() {
+import Inventory from '../Inventory';
+import Sales from '../Sales';
+import Home from '../Home';
+
+import { clearState } from '../../redux/features/authSlice';
+
+const drawerWidth = 280;
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+  },
+  drawer: {
+    [theme.breakpoints.up('lg')]: {
+      width: drawerWidth,
+      flexShrink: 0,
+    },
+  },
+  toolbar: theme.mixins.toolbar,
+  content: {
+    flexGrow: 1,
+    padding: theme.spacing(3),
+    backgroundColor: 'rgb(248, 248, 248)',
+  },
+}));
+
+/* eslint-disable react/prop-types */
+const Dashboard = (props) => {
+  const [mobileOpen, setMobileOpen] = useState(false);
   const dispatch = useDispatch();
-  const history = useHistory();
+  const classes = useStyles();
+  const { match } = props;
 
   useEffect(() => {
     dispatch(clearState());
   }, []);
 
-  const handleSignOut = async () => {
-    dispatch(verifying());
-    await auth.signOut();
-    dispatch(clearActiveUser());
-    dispatch(verified());
-    history.push('/auth/login');
+  const handleDrawerToggle = () => {
+    setMobileOpen(!mobileOpen);
   };
 
   return (
-    <div>
-      Dashboard Page
-      <Button variant="text" color="primary" onClick={handleSignOut}>
-        Sign Out
-      </Button>
+    <div className={classes.root}>
+      <AppBars handleDrawerToggle={handleDrawerToggle} />
+      <nav className={classes.drawer}>
+        <Drawer
+          type="Mobile"
+          status={mobileOpen}
+          handleDrawerToggle={handleDrawerToggle}
+        />
+        <Drawer type="Desktop" />
+      </nav>
+      <main className={classes.content}>
+        <div className={classes.toolbar} />
+        <Switch>
+          <Route path={`${match.path}`} exact component={Home} />
+          <Route path={`${match.path}/inventory`} component={Inventory} />
+          <Route path={`${match.path}/sales`} component={Sales} />
+        </Switch>
+      </main>
     </div>
   );
-}
+};
+
+export default withRouter(Dashboard);

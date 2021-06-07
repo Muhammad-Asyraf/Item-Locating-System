f253234326c0d0382020e183bfcbb1d5112f643e
asyraf.rmc <asyraf.rmc@gmail.com>
2021-06-07 15:19:30 +0800

    Setup Inventory Page #LOKET-87

--- src/client/src/components/Inventory.js
+++ src/client/src/components/Inventory.js
@@ -1,112 +1,21 @@
 import React from 'react';
+import { withRouter, Switch, Route } from 'react-router-dom';
 
-const Inventory = () => (
-  <div>
-    <h1>This is Inventory Page</h1>{' '}
-    <p>
-      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ut harum, dolores
-      repellendus quo eum doloribus, dolorem maiores porro necessitatibus impedit
-      quibusdam et laboriosam, ea natus. Minima quod exercitationem veritatis non
-      inventore totam assumenda unde nam explicabo aut, reprehenderit perspiciatis
-      consequuntur animi, repudiandae voluptatem sit nihil beatae at culpa rem
-      veniam. Suscipit nulla, earum nam deserunt soluta ullam cum dicta enim,
-      temporibus vitae perferendis id, ea nesciunt aut quaerat laudantium? Ea, quis
-      obcaecati necessitatibus quaerat tempore amet at assumenda pariatur error
-      fugiat, blanditiis ipsam delectus repellendus deleniti ut corporis soluta
-      asperiores. Quae quas et molestiae minima adipisci architecto sed, inventore ab
-      at cupiditate numquam amet voluptatibus doloribus deserunt explicabo, mollitia
-      magni nobis saepe. Officia obcaecati assumenda architecto facilis doloremque,
-      sed quibusdam voluptates ipsum, accusantium numquam magni cupiditate vero
-      dignissimos iste ullam adipisci enim iure excepturi veritatis! Saepe, ut.
-      Repudiandae error quisquam harum? Omnis pariatur provident vitae! Corrupti at
-      sed cupiditate ipsum nulla iste hic unde in, quaerat inventore molestiae quas
-      amet laudantium assumenda, quam, obcaecati dolore consequatur omnis sint sunt
-      atque est eius? Sit vitae placeat doloremque. Odio aut omnis nobis pariatur, ex
-      deleniti, explicabo ratione tempore est dolorum alias ipsum minus dolore, earum
-      molestiae fugiat quisquam quos neque non. Nihil, dignissimos pariatur neque
-      obcaecati optio provident repellendus modi reprehenderit dolore quo, quam quae
-      ea et temporibus laborum cumque ut? Possimus accusantium quis corrupti, eos ex
-      inventore, magnam consequatur sapiente debitis facere doloremque illo nam
-      laborum quasi! Dignissimos obcaecati dolore voluptas nisi consequatur veniam
-      officiis aspernatur dicta corporis doloribus. Quasi neque id quidem dolor ea
-      dolorem corporis rem atque tempore. Et itaque explicabo saepe illum
-      perspiciatis iusto nostrum! Debitis reiciendis error, laudantium eum minima
-      quia eos, illum, corporis illo tenetur eius omnis voluptates saepe ut officiis
-      atque exercitationem ab dolor tempora accusamus quasi dolorum hic! Natus
-      accusamus ut excepturi voluptas earum veritatis delectus. Consequuntur, amet
-      harum. Suscipit inventore eos dolorum soluta a eius cum iure accusamus.
-      Assumenda enim vel ex perspiciatis magni sequi excepturi deserunt atque quaerat
-      qui iste beatae, quisquam veritatis dolorem ad omnis necessitatibus asperiores
-      earum aut nam sapiente voluptatum accusamus animi alias! Voluptatem eligendi
-      labore atque quis modi debitis explicabo nemo, doloremque, ut nulla,
-      consequatur id sit iusto quaerat voluptatum nisi nihil voluptate sequi autem
-      laborum ipsa quia quos. Beatae impedit ut delectus consequatur ad reiciendis
-      doloribus nemo labore nostrum sed. Fuga praesentium numquam blanditiis
-      accusantium, officiis quasi! Officia deleniti esse eos vitae repellendus
-      doloribus quidem libero hic quaerat pariatur dolorum, dolorem illo aut,
-      blanditiis reprehenderit suscipit. Ratione deserunt officia perferendis sed
-      accusamus rem ullam nostrum sit aliquam architecto, quae cupiditate quisquam.
-      Provident nobis adipisci dolor magnam cum aut voluptatum doloremque quos quod?
-      Rem libero repellat quis magni sint sit, mollitia cupiditate quo veniam impedit
-      ullam omnis aliquam, eaque ut ducimus nihil illo modi sed nam molestiae
-      accusamus. Consequatur dolore, ad, voluptates, maxime totam delectus ut eos
-      nisi perferendis aperiam molestiae rerum hic unde minima vel magni voluptatibus
-      voluptas illo vitae sunt optio. Quaerat, in, numquam nam vel suscipit aperiam
-      cupiditate repellat soluta porro nesciunt consequatur. Dolore, id.
-    </p>
-    <p>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque assumenda earum
-      suscipit beatae aliquam ad. Voluptatem officiis consectetur quia explicabo
-      distinctio possimus sint ullam sunt, veritatis numquam dolores deserunt
-      repudiandae at expedita consequatur similique soluta rerum debitis delectus
-      reiciendis! Architecto consequuntur eligendi itaque illo quis veniam ab ullam
-      ducimus, nihil est asperiores, omnis doloremque, fugiat dicta a doloribus nemo
-      atque. In sit architecto eos sequi amet pariatur iure ut voluptatibus
-      laboriosam omnis. Nulla cupiditate corporis excepturi molestiae facilis quis
-      velit ut consequatur, amet eveniet. Fugiat beatae eaque vitae explicabo
-      suscipit minus exercitationem. Veniam, mollitia a aut voluptatum,
-      exercitationem, voluptatem aperiam ipsum fugiat repellendus assumenda quam
-      adipisci ex consequuntur cum? Eius, dolorem. Dicta rerum mollitia laborum totam
-      explicabo ex ad est dolores voluptas a saepe perspiciatis laboriosam aperiam
-      ratione quo, aliquam soluta commodi maxime et odio ab! Illum et nostrum earum
-      non voluptates dolorum autem tenetur nam, harum obcaecati exercitationem
-      tempora veritatis nesciunt voluptatem animi! Fuga a dolore odit nostrum
-      suscipit perferendis similique eos excepturi! Aspernatur placeat asperiores
-      harum pariatur vel sit debitis labore deleniti mollitia eos. Culpa dolorem
-      ullam in quis harum! Quia ad sit tenetur, accusantium, sed earum eveniet
-      consequuntur excepturi iste aliquam veniam praesentium voluptate maiores unde
-      et repellat minus, laborum sequi modi omnis mollitia temporibus. Dolorum dolor
-      vero, iure nostrum enim tempore sed tenetur. Quod veniam quo non asperiores
-      dolor itaque aut sit dolorem, commodi repellat sapiente provident eaque
-      praesentium quam nulla dicta officia delectus repellendus possimus totam
-      placeat quisquam quidem voluptates? Praesentium consectetur quo voluptates?
-      Dicta nisi eveniet perspiciatis temporibus error praesentium nemo distinctio
-      cumque porro! Ipsam facilis debitis corrupti suscipit voluptates eaque eos
-      assumenda dolorum quasi, a id dolorem placeat saepe necessitatibus neque qui
-      voluptatum illum. Repudiandae reprehenderit quia doloribus quibusdam numquam
-      minima. Sequi corporis nisi quibusdam ipsam neque maiores ab libero similique,
-      soluta enim quas ea, sed dolores nam ipsa error, eius expedita illum nihil
-      amet. Modi rem quo soluta ab voluptas nam tenetur recusandae praesentium ipsam,
-      officiis accusamus beatae. Inventore harum rem non odio reiciendis, earum nam,
-      hic, nemo eius accusantium repellat vero autem deserunt voluptatem? Consequatur
-      autem ab voluptate iste qui beatae cupiditate magni ut, fugit, suscipit maxime,
-      est dicta doloremque placeat nemo ullam tenetur soluta tempore rem! Repellat
-      animi nostrum reprehenderit omnis nesciunt facilis dignissimos blanditiis?
-      Maiores deserunt non amet dolorem autem voluptas? Ipsum totam ad nulla incidunt
-      sapiente. Molestias accusamus corrupti numquam ipsa tempora magnam nam
-      praesentium quis, recusandae natus consequuntur voluptatibus aperiam magni
-      officiis! Praesentium quia non, omnis itaque eveniet hic aliquam ipsa quo.
-      Optio quas suscipit assumenda similique quod quis, asperiores quae mollitia
-      voluptatem. Dolor neque laudantium aliquam autem. Fuga quibusdam molestiae
-      ipsam voluptatum harum porro beatae adipisci aliquid dignissimos, omnis
-      consectetur facere qui nihil veniam ducimus voluptas iure repudiandae
-      aspernatur odit, repellat commodi ullam quidem tempora? Doloremque illo ipsa
-      repellendus ea assumenda, tempore suscipit non minima omnis, excepturi dolorem
-      pariatur necessitatibus. Suscipit asperiores, possimus ipsam perspiciatis
-      pariatur tempora ullam molestiae nulla repellat quibusdam? Accusantium non
-      pariatur minus aliquam amet debitis! Voluptate nihil ipsum ipsa, ab id odio.
-    </p>
-  </div>
-);
+import InventoryList from './InventoryList/InventoryList';
+import InventoryItem from './InventoryItem';
+import InventoryCreate from './InventoryCreate';
 
-export default Inventory;
+const Inventory = (props) => {
+  const { match } = props;
+
+  return (
+    <>
+      <Switch>
+        <Route path={`${match.path}/list`} exact component={InventoryList} />
+        <Route path={`${match.path}/item`} exact component={InventoryItem} />
+        <Route path={`${match.path}/create`} exact component={InventoryCreate} />
+      </Switch>
+    </>
+  );
+};
+export default withRouter(Inventory);

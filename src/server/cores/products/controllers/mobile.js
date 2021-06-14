1e90d140c581dfeeac730b47b7d2623d239062c5
danishrashidin <danishrashidin@gmail.com>
2021-06-15 02:12:05 +0800

    Added search function in backend #LOKET-107

---
+++ src/server/cores/products/controllers/mobile.js
@@ -0,0 +1,17 @@
+const { v4: uuidv4 } = require('uuid');
+const Product = require('../model');
+const getLogger = require('../../../utils/logger');
+
+const productLogger = getLogger(__filename, 'product');
+
+exports.searchProducts = async (req, res, next) => {
+    try {
+        const { search } = req.query
+        const products = await Product.query().where('name','ilike',`%${search}%`)
+        productLogger.info(`Successfully retrieve: ${products.length} products on ${search}`);
+        res.json(products);
+      } catch (err) {
+        productLogger.warn(`Error retrieving all products`);
+        next(err);
+      }
+}

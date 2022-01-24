db27e76c679150f5a7b65129cfcc5384a030a45d
danishrashidin <danishrashidin@gmail.com>
2022-01-24 16:21:51 +0800

    Conflict fixes #hotFix

--- src/server/cores/products/controllers/mobile.js
+++ src/server/cores/products/controllers/mobile.js
@@ -41,55 +41,6 @@ exports.searchProducts = async (req, res, next) => {
       .where('name', 'ilike', `%${query.search}%`)
       .withGraphFetched('stores');
     res.json(products);
-
-    // let products;
-    // if ('uuid' in query && query.uuid != '') {
-    //   products = await Product.query()
-    //     .where('store_uuid', query.uuid)
-    //     .where('is_active', true)
-    //     .where('name', 'ilike', `%${query.search}%`)
-    //     .withGraphFetched('stores');
-
-    //   // products = await Store.relatedQuery('products')
-    //   //   .for(query.uuid)
-    //   //   .where('name', 'ilike', `%${query.search}%`);
-    //   productLogger.info(
-    //     `Successfully retrieve: ${products.length} products on ${query.search} from store uuid <${query.uuid}>`
-    //   );
-    //   res.json(products);
-    // } else {
-    //   products = await Product.query()
-    //     .where('is_active', true)
-    //     .where('name', 'ilike', `%${query.search}%`)
-    //     .withGraphFetched('stores');
-    //   productLogger.info(
-    //     `Successfully retrieve: ${products.length} products on ${query.search}`
-    //   );
-    //   res.json(products);
-    // }
-    let products;
-    if ('uuid' in query && query.uuid != '') {
-      products = await Product.query()
-        .where('store_uuid', query.uuid)
-        .where('name', 'ilike', `%${query.search}%`)
-        .withGraphFetched('stores');
-
-      // products = await Store.relatedQuery('products')
-      //   .for(query.uuid)
-      //   .where('name', 'ilike', `%${query.search}%`);
-      productLogger.info(
-        `Successfully retrieve: ${products.length} products on ${query.search} from store uuid <${query.uuid}>`
-      );
-      res.json(products);
-    } else {
-      products = await Product.query()
-        .where('name', 'ilike', `%${query.search}%`)
-        .withGraphFetched('stores');
-      productLogger.info(
-        `Successfully retrieve: ${products.length} products on ${query.search}`
-      );
-      res.json(products);
-    }
   } catch (err) {
     productLogger.warn(`Error retrieving all products`);
     next(err);
